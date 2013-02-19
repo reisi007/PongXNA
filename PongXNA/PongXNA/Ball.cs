5dd5f7306b750c3f75344f823dae3c744ff83989
Florian Reisinger <reisi007@gmail.com>
2013-02-19 12:10:42 +0100

    Initial Commit

---
+++ PongXNA/PongXNA/Ball.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PongXNA
+{
+   public class Ball
+    {
+        Player Player;
+        int current_index = 0;
+        Vector2 Position, Direction;
+        Color[][] Spherecol = new Color[2][];
+        Texture2D Texture;
+        int m_sec_change_graphic = 120;
+        int m_sec_passed = 0;
+        float sphere_size = 50;
+        SpriteBatch batch;
+        public Ball(Vector2 dir,Vector2 pos, Texture2D ball, SpriteBatch spritebatch)
+        {
+            Texture = ball;
+            batch = spritebatch;
+            Direction = dir;
+            Position = pos;
+            if( dir.X > 0)
+                Player = Player.Red;
+            else
+                Player = Player.Blue;
+            # region SphereColorArray Init
+            Spherecol[0] = new Color[6];
+            Spherecol[1] = new Color[6];
+
+            Spherecol[0][0] = new Color(3, 17, 172);
+            Spherecol[0][1] = new Color(4, 20, 197);
+            Spherecol[0][2] = new Color(4, 22, 222);
+            Spherecol[0][3] = new Color(5, 25, 247);
+            Spherecol[0][5] = new Color(4, 20, 197);
+            Spherecol[0][4] = new Color(4, 22, 222);
+            
+            Spherecol[1][0] = new Color(172, 3, 17);
+            Spherecol[1][1] = new Color(197, 4, 20);
+            Spherecol[1][2] = new Color(222, 4, 22);
+            Spherecol[1][3] = new Color(247, 5, 25);
+            Spherecol[1][5] = new Color(197, 4, 20);
+            Spherecol[1][4] = new Color(222, 4, 22);
+            #endregion
+
+
+        }
+        public void Update(GameTime gametime)
+        {
+            m_sec_passed += gametime.ElapsedGameTime.Milliseconds;
+            if (m_sec_passed > m_sec_change_graphic)
+            {
+                m_sec_passed -= m_sec_change_graphic;
+                current_index++;
+                current_index %= 6;
+            }
+        }
+        public void Draw()
+        {
+            batch.Draw(Texture, new Rectangle((int)(Position.X - 25),(int)(Position.Y - 25), 50, 50), Spherecol[(int)Player][current_index]);
+        }
+    }
+}
---
+++ PongXNA/PongXNA/Game1.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PongXNA
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+     public enum Player { Red = 1, Blue = 0 }
+
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D background;
+        List<Ball> Balls = new List<Ball>();
+        Texture2D ball_img;
+        Vector2 spawn_pos;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            background = Content.Load<Texture2D>("grass");
+            ball_img = Content.Load<Texture2D>("ball");
+# if DEBUG
+            graphics.PreferredBackBufferHeight = 800;
+            graphics.PreferredBackBufferWidth = 1800;
+#else
+
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.ToggleFullScreen();
+#endif
+            graphics.ApplyChanges();
+            spawn_pos = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Balls.Add(new Ball(new Vector2(-1, 1),spawn_pos, ball_img, spriteBatch));
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+            foreach (Ball b in Balls)
+                b.Update(gameTime);
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            // Draw background
+            for (int x = 0; x < Window.ClientBounds.Width; x += background.Width)
+                for (int y = 0; y < Window.ClientBounds.Height; y += background.Height)
+                    spriteBatch.Draw(background, new Vector2(x, y), Color.White);
+            foreach (Ball b in Balls)
+                b.Draw();
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
